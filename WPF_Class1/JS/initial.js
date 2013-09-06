4155986de72fd5428caba8d8fd0afe332baf204c
cabraldiogo <cabraldiogo@me.com>
2013-09-05 21:52:54 -0400

    ReorganizationOfFiles
    I deleted a test file and synced for the first time

---
+++ WPF_Class1/JS/initial.js
@@ -0,0 +1,46 @@
+//var initialValue= 1;
+//
+//console.log(initialValue);
+//
+//initialValue =initialValue +1;
+//initialValue*=5;
+//
+//
+//console.log(initialValue);
+
+
+//var len = 2;
+//var wid = 3;
+//var height = 2;
+//
+//var area =  len*wid*height;
+//
+//console.log(area);
+//
+//
+//var R= 10;
+//var CircArea= R*R*Math.PI
+//console.log (CircArea) ;
+
+//5% salary, increase annualy for 3 years//
+
+var salary= prompt('Enter salary')*1;
+var i= 0.05;
+var year1 = (salary+salary*i);
+
+var year2 = (year1+year1*i);
+
+var year3 = (year2+year2*i);
+
+console.log(year1+' '+ year2+' '+year3);
+
+
+
+
+
+
+
+
+
+
+
