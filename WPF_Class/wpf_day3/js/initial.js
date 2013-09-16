0536ee4ea8b84de288960a3ecbbb9d3e12fd7f04
cabraldiogo <cabraldiogo@me.com>
2013-09-16 18:34:14 -0400

    FolderNameChange
    I changed the folder WPF_Class1 to WPF_Class

---
+++ WPF_Class/wpf_day3/js/initial.js
@@ -0,0 +1,37 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: diogocabral
+ * Date: 9/16/13
+ * Time: 12:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+
+var numberVariable= 10+'';
+var stringVariable='happy';
+var otherNumberVariable=15;
+var concatVariable = numberVariable*1 + otherNumberVariable;
+console.log(concatVariable);
+
+
+
+//conditionals example
+   var hitPoints=0;
+  var result ='';
+if(hitPoints<=0){
+   result = 'You are super dead.';
+   }
+else{
+    result='You are alive.'
+}
+console.log(result);
+
+
+
+
+function myFirstFunction (number1, number2){
+
+    var
+
+}
