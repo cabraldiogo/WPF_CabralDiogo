e176f5a250bb5bb4e36076714b2df4e0e8c0f7a7
cabraldiogo <cabraldiogo@me.com>
2013-09-26 22:15:50 -0400

    commentsOnCode
    I just commented all the industry type assignment

--- WPF_Class/Assignments/functions_assignment/js/industry.js
+++ WPF_Class/Assignments/functions_assignment/js/industry.js
@@ -9,27 +9,32 @@
 
 //    DIOGO CABRAL - FUNCTIONS ASSIGNMENT
 
-
+//Input for last name
 var lastName = prompt("Enter you last name");
 
+//input for first name
 var firsttName = prompt("Enter you first name");
 
+//input with options for the degreem program
 var degree = prompt("Enter the number that correspond to your degree program: Web Design : 1  | Film : 2  | Recording Arts: 3");
 
+//array with the launchbox costs
 var launchBox = ["2000","2500","3000"];
 
+//array that contain the programs name
 var programs = [];
     programs[0] = "Web Design";
     programs[1] = "Film";
     programs[2] = "Recording Arts";
 
-
+//function that call last name and first name and print it as First Name + Last Name
 function studentInfo(lastName,firstName){
     return firstName + lastName
 }
 console.log(studentInfo(lastName,firsttName));
 
-
+//If conditionals to check the degree chosen by the user. Depending on each program will print a different launch box cost and it
+//will also call the program name array
 if  (degree < 1 || degree >3)
 {alert("You should enter the options 1, 2 or 3, according to the desired program. Refresh you page and try again");
 
--- WPF_Class/Assignments/functions_assignment/js/personal.js
+++ WPF_Class/Assignments/functions_assignment/js/personal.js
@@ -9,26 +9,7 @@
 
 //    DIOGO CABRAL - FUNCTIONS ASSIGNMENT
 
-var monthInput = parseInt(prompt("Enter month"));
 
-
-var months = [];
-months[0] = "January";
-months[1] = "February";
-months[2] = "March";
-months[3] = "Abril";
-months[4] = "May";
-months[5] = "June";
-months[6] = "July";
-months[7] = "August";
-months[8] = "September";
-months[9] = "October";
-months[10] = "November";
-months[11] = "December";
-
-
-
-console.log(months);
 
 
 
