719fddea19c1631c88afe6591db6d2970318f07a
cabraldiogo <cabraldiogo@me.com>
2013-09-26 22:10:59 -0400

    functionsAssignemtn
    I finish the first one and I will comment all the lines

--- WPF_Class/Assignments/functions_assignment/js/industry.js
+++ WPF_Class/Assignments/functions_assignment/js/industry.js
@@ -25,23 +25,22 @@ var programs = [];
 
 
 function studentInfo(lastName,firstName){
-
     return firstName + lastName
-
 }
-
 console.log(studentInfo(lastName,firsttName));
 
 
 if  (degree < 1 || degree >3)
-{alert("You should enter the options 1, 2 or 3, according to the desired program");
+{alert("You should enter the options 1, 2 or 3, according to the desired program. Refresh you page and try again");
 
-}else if (degree == 1){
+}if (degree == 1){
     console.log("The cost of the " + programs[0] + " launchbox is $" + launchBox[0]);
 } else if (degree == 2){
     console.log("The cost of the " + programs[1] + " launchbox is $" + launchBox[1]);
-} else {
+} else if (degree == 3){
     console.log("The cost of the " + programs[2] + " launchbox is $" + launchBox[2]);
+} else {
+
 }
 
 
