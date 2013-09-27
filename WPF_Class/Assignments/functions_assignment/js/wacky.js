80daeb6093b3527ffeaac031eafa3aad4b96154d
cabraldiogo <cabraldiogo@me.com>
2013-09-26 23:47:04 -0400

    wacky
    I just tested the wacky type is working fine. Is a calculator for a
    purchase order, that gives a free shipping on orders over $50 and a 10%
    discount if the customer has a discount cupom

--- WPF_Class/Assignments/functions_assignment/js/wacky.js
+++ WPF_Class/Assignments/functions_assignment/js/wacky.js
@@ -10,3 +10,31 @@
 //    DIOGO CABRAL - FUNCTIONS ASSIGNMENT
 
 
+var products = parseInt(prompt("Enter the total value of your purchase"));
+var discountCupom = (prompt("If you have a discount cupom, enter the code here. Otherwise leave it blank"));
+
+console.log("Your order details");
+console.log("Sub-total $" + products);
+
+if(products > 50){
+        shippingCost = 0;
+}else{
+        shippingCost = 7;
+}
+
+if(discountCupom == "promo"){
+    products = products * 0.9
+
+}else {
+    products = products*1
+}
+
+
+var grandTotal = products + shippingCost;
+
+
+console.log("Sub-total with discount $" + products);
+console.log("Shipping Cost $" + shippingCost);
+console.log("Your order grand total is $" + grandTotal);
+
+
