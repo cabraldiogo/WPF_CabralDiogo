d83674a1dfd1173ec99a833b4a6cc769c003d6a1
cabraldiogo <cabraldiogo@me.com>
2013-10-01 09:08:38 -0400

    WPF_lastClassActivity
    Class exercises

--- WPF_Class/wpf_day9/js/init.js
+++ WPF_Class/wpf_day9/js/init.js
@@ -1,63 +1,133 @@
-/**
- * Created with JetBrains WebStorm.
- * User: diogocabral
- * Date: 9/27/13
- * Time: 5:23 PM
- * To change this template use File | Settings | File Templates.
- */
+///**
+// * Created with JetBrains WebStorm.
+// * User: diogocabral
+// * Date: 9/27/13
+// * Time: 5:23 PM
+// * To change this template use File | Settings | File Templates.
+// */
+//
+//function average(salesMonth1,salesMonth2,salesMonth3){
+//
+//    return (salesMonth1 + salesMonth2 + salesMonth3) / 3
+//
+//}
+//
+//function salesAverage(avg){
+//
+//    return (avg[0]+avg[1]+avg[2])/3
+//
+//}
+//
+//
+//function salesIncreaseFunction(avg, salesIncrease){
+//
+//    return ((avg[0]+avg[1]+avg[2])/3) + (avg[0]+avg[1]+avg[2])/3 *salesIncrease;
+//
+//}
+//
+//
+//
+//var salesMonth1 = 200;
+//var salesMonth2 = 375;
+//var salesMonth3 = 320;
+//
+//var sales = [];
+//    sales[0] = 200;
+//    sales[1] = 200;
+//    sales[2] = 200;
+//
+//var increaseSales = 0.15;
+//var current = 150;
+//var average1 = salesMonth1 + salesMonth2 + salesMonth3 / 3;
+//
+//
+//
+//
+//console.log(average(salesMonth1,salesMonth2,salesMonth3));
+//
+//console.log(salesIncreaseFunction(sales,increaseSales));
+//
+//console.log(salesAverage(sales));
+//
+//function performanceFunction(performance, aver){
+//
+//        return performance / aver
+//
+//
+//
+//}
+//
+//console.log(performanceFunction(current,average1));
 
-function average(salesMonth1,salesMonth2,salesMonth3){
-
-    return (salesMonth1 + salesMonth2 + salesMonth3) / 3
-
-}
-
-function salesAverage(avg){
-
-    return (avg[0]+avg[1]+avg[2])/3
-
-}
 
 
-function salesIncreaseFunction(avg, salesIncrease){
+//var largest = 0;
+//var numbers = [1,3,4,3,5,7,9,7,3,3,2,6];
+//
+//function maxNumber(larg){
+//    for(i=0; i < larg.length;i++){
+//        if(largest < larg[i]){
+//            largest = larg[i];
+//
+//
+//        }
+//
+//
+//    }
+//    return largest
+//
+//
+//
+//}
+//
+//console.log(maxNumber(numbers));
+//
+//
+//
+//var currentSales = 70;
+//var maxSales = 200;
+//var minSales = 100;
+//
+//
+//function flag(current,min,max){
+//    var message = "";
+//
+//    if(current > max){
+//        message = "You Rock!"
+//    }else if(current < min){
+//        message = "You're fired. Get out of here!"
+//    }else {message = "You are ok, but work harder"}
+//    return message
+//}
+//
+//
+//console.log(flag(currentSales,minSales,maxSales));
 
-    return ((avg[0]+avg[1]+avg[2])/3) + (avg[0]+avg[1]+avg[2])/3 *salesIncrease;
-
-}
+//Last day class
 
 
 
-var salesMonth1 = 200;
-var salesMonth2 = 375;
-var salesMonth3 = 320;
+var names = ["Diogo Cabral","Joe Dalton"];
+var grade = 100;
 
-var sales = [];
-    sales[0] = 200;
-    sales[1] = 200;
-    sales[2] = 200;
+// "\t" = insert a tab space
+// "\n" = line break
 
-var increaseSales = 0.15;
-var current = 150;
-var average1 = salesMonth1 + salesMonth2 + salesMonth3 / 3;
+console.log(names[0] + "\t" + grade + "\n");
+console.log(names[1] + "\t\t" + grade);
 
 
 
 
-console.log(average(salesMonth1,salesMonth2,salesMonth3));
-
-console.log(salesIncreaseFunction(sales,increaseSales));
-
-console.log(salesAverage(sales));
-
-function performanceFunction(performance, aver){
-
-        return performance / aver
 
 
 
-}
 
-console.log(performanceFunction(current,average1));
+
+
+
+
+
 
 
 
